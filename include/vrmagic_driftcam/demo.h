3aa931914832ff14d240b7519b557abfb2ed93df
Miquel Massot <miquel.massot@gmail.com>
2018-10-31 08:07:19 -0700

    Init commit

---
+++ include/vrmagic_driftcam/demo.h
@@ -0,0 +1,41 @@
+// ==============================================================================================
+// This file is part of the VRmagic USB2.0 Camera Demo Application
+// ==============================================================================================
+#ifndef VRMUSBCAMDEMO_H
+#define VRMUSBCAMDEMO_H
+
+// include the api of the library
+#include  <vrmusbcam2.h>
+#ifdef WIN32
+#include <vrmusbcam2win32.h>
+#endif
+
+// function prototypes "main.cpp"
+void LogExit();
+
+// small helper macro to check function for success and call LogExit when function fails
+#define VRMEXECANDCHECK(function)\
+{\
+	if (VRM_SUCCESS!=function)\
+	LogExit();\
+}
+
+// function prototypes "readcamera.cpp"
+void getNextTuple(VRmUsbCamDevice device, VRmDWORD num_ports, VRmBOOL* active_port_list, VRmImage** p_source_img);
+void readCamera(VRmUsbCamDevice device, VRmDWORD num_ports, VRmBOOL* active_port_list, VRmImageFormat* target_format);
+
+// function prototypes "initcamera.cpp"
+void initCamera(VRmUsbCamDevice device, VRmDWORD& num_ports, VRmBOOL* active_port_list, VRmImageFormat* target_format);
+
+// function prototypes "sdlwindow.cpp"
+bool SDLWindowInit( const char* fp_caption, VRmImageFormat f_format);
+void SDLWindowClose();
+unsigned char* SDLLockBuffer( unsigned int& f_pitch );
+void SDLUnlockBuffer();
+void SDLUpdate();
+bool kbhit();
+
+// global variables "sdlwindow.cpp"
+extern bool g_use_sysmem;
+
+#endif //VRMUSBCAMDEMO_H
